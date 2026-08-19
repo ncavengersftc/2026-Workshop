899a9f3f04ddedd27f9d87b28cfa717fca963e53
Adi <varman.shadow@gmail.com>
2026-08-19 17:34:45 -0400

    built base code of opmode and built shooter subsystem

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Shooter.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode;
+
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.seattlesolvers.solverslib.command.SubsystemBase;
+
+public class Shooter extends SubsystemBase {
+    private final DcMotor outtakeMotorLeft;
+    private final DcMotor outtakeMotorRight;
+    public Shooter(){
+        outtakeMotorLeft = hardwareMap.get(DcMotor.class, "Outtake Motor Left");
+        outtakeMotorRight = hardwareMap.get(DcMotor.class, "Outtake Motor Right");
+    }
+    public void shoot(){
+        outtakeMotorLeft.setPower(0.25);
+        outtakeMotorRight.setPower(-0.25);
+        }
+    public void stop(){
+        outtakeMotorLeft.setPower(0);
+        outtakeMotorLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        outtakeMotorRight.setPower(0);
+        outtakeMotorRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+    }
+    public void pull(){
+        outtakeMotorLeft.setPower(-0.25);
+        outtakeMotorRight.setPower(0.25);
+    }
+    }
