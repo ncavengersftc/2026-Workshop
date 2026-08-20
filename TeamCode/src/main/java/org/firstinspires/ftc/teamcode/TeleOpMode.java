8e726c9df46f772de673cc459436ed6a15793b71
Isha Kannan <kakeisha146@gmail.com>
2026-08-20 15:40:16 -0400

    code the shooter motors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMode.java
@@ -6,9 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 @TeleOp
 public class TeleOpMode extends LinearOpMode {
     private final Shooter shooter;
-    public TeleOpMode (){
-        shooter = new Shooter();
-    }
+    public TeleOpMode () { shooter = new Shooter(); }
     @Override
     public void runOpMode(){
         waitForStart();
